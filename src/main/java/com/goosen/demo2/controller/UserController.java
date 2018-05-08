096e070d0a0aacb6295632a32bf027456e434157
goosen123 <2630344884@qq.com>
2018-05-08 19:04:29 +0800

    提交文件

---
+++ src/main/java/com/goosen/demo2/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.goosen.demo2.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户Controller
+ * @author Goosen
+ * @version 2018-05-08
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController extends BaseController {
+	
+	protected Logger log = LoggerFactory.getLogger(getClass());
+
+	@RequestMapping(value = {"list"})
+	public void list(HttpServletRequest request, HttpServletResponse response, Model model) {
+		
+		log.info("进来了<<<<<<<<<<<<<<<<<<<<");
+		
+		return;
+//		return "modules/test/success";
+	}
+	
+}
